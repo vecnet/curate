9031029342209d43776234b8088d7e2d77018fc9
Banurekha <banurekha.l@gmail.com>
2014-01-16 13:48:14 -0500

    when there is failure with full text extract, store status code and message as content to full text

--- app/repository_models/curation_concern/with_full_text.rb
+++ app/repository_models/curation_concern/with_full_text.rb
@@ -11,7 +11,7 @@ module CurationConcern
       uri = URI(url+'/update/extract?extractOnly=true&wt=ruby&extractFormat=text')
       req = Net::HTTP.new(uri.host, uri.port)
       resp = req.post(uri.to_s, self.content.content, {'Content-type'=>self.mime_type+';charset=utf-8', "Content-Length"=>"#{self.content.content.size}" })
-      extracted_text = eval(resp.body)[""].rstrip
+      extracted_text = resp.code.eql?("200") ? eval(resp.body)[""].rstrip : "#{resp.code}:#{resp.message}"
       self.full_text.mimeType='text/plain'
       self.full_text.content = extracted_text if extracted_text.present?
       save unless self.new_object?
